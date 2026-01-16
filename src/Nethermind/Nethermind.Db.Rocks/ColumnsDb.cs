e2bf1bc55bad36a6e38cdb69a7c5afc6f135d766
Amirul Ashraf <asdacap@gmail.com>
2026-01-16 08:29:37 +0800

    FlatDB squashed commit

--- src/Nethermind/Nethermind.Db.Rocks/ColumnsDb.cs
+++ src/Nethermind/Nethermind.Db.Rocks/ColumnsDb.cs
@@ -4,8 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using FastEnumUtility;
 using Nethermind.Core;
+using Nethermind.Core.Extensions;
 using Nethermind.Db.Rocks.Config;
 using Nethermind.Logging;
 using RocksDbSharp;
--- src/Nethermind/Nethermind.Db.Rocks/DbOnTheRocks.cs
+++ src/Nethermind/Nethermind.Db.Rocks/DbOnTheRocks.cs
@@ -224,7 +224,6 @@ public partial class DbOnTheRocks : IDb, ITunableDb, IReadOnlyNativeKeyValueStor
             CreateMarkerIfCorrupt(x);
             throw;
         }
-
     }
 
     private void WarmupFile(string basePath, RocksDb db)
@@ -445,7 +444,7 @@ public partial class DbOnTheRocks : IDb, ITunableDb, IReadOnlyNativeKeyValueStor
         return 0;
     }
 
-    [GeneratedRegex("(?<optionName>[^; ]+)\\=(?<optionValue>[^; ]+);", RegexOptions.Singleline | RegexOptions.NonBacktracking | RegexOptions.ExplicitCapture)]
+    [GeneratedRegex("(?<optionName>[A-Za-z0-9_\\.]+)\\=(?<optionValue>[^; ]+);", RegexOptions.Singleline | RegexOptions.NonBacktracking | RegexOptions.ExplicitCapture)]
     private static partial Regex ExtractDbOptionsRegex();
 
     public static IDictionary<string, string> ExtractOptions(string dbOptions)
@@ -908,6 +907,12 @@ public partial class DbOnTheRocks : IDb, ITunableDb, IReadOnlyNativeKeyValueStor
         }
     }
 
+    internal Span<byte> GetSpanWithColumnFamily(scoped ReadOnlySpan<byte> key, ColumnFamilyHandle? cf, ReadFlags flags)
+    {
+        ReadOptions options = (flags & ReadFlags.HintCacheMiss) != 0 ? _hintCacheMissOptions : _defaultReadOptions;
+        return GetSpanWithColumnFamily(key, cf, options);
+    }
+
     internal Span<byte> GetSpanWithColumnFamily(scoped ReadOnlySpan<byte> key, ColumnFamilyHandle? cf, ReadOptions readOptions)
     {
         ObjectDisposedException.ThrowIf(_isDisposing, this);
@@ -1146,6 +1151,25 @@ public partial class DbOnTheRocks : IDb, ITunableDb, IReadOnlyNativeKeyValueStor
 
         Iterator iterator = CreateIterator(ordered);
         return GetAllValuesCore(iterator);
+    }
+
+    public IDbSnapshot CreateSnapshot()
+    {
+        Snapshot snapshot = _db.CreateSnapshot();
+
+        ReadOptions readOptions = new();
+        readOptions.SetSnapshot(snapshot);
+
+        ReadOptions readCachedOptions = new();
+        readCachedOptions.SetSnapshot(snapshot);
+        readCachedOptions.SetReadTier(1); // Cache tier
+
+        return new DbSnapshot(
+            this,
+            readOptions,
+            readCachedOptions,
+            null,
+            snapshot);
     }
 
     internal IEnumerable<byte[]> GetAllValuesCore(Iterator iterator)
@@ -2042,6 +2066,144 @@ public partial class DbOnTheRocks : IDb, ITunableDb, IReadOnlyNativeKeyValueStor
         Snapshot snapshot
     ) : RocksDbReader(mainDb, readOptionsFactory, null, columnFamily), IKeyValueStoreSnapshot
     {
+        public void Dispose()
+        {
+            snapshot.Dispose();
+        }
+    }
+
+    public class DbSnapshot(
+        DbOnTheRocks mainDb,
+        ReadOptions options,
+        ReadOptions cachedOptions,
+        ColumnFamilyHandle? columnFamily,
+        Snapshot snapshot
+    ) : IDbSnapshot, ISortedKeyValueStore, ICacheOnlyReader
+    {
+
+        public byte[]? Get(scoped ReadOnlySpan<byte> key, ReadFlags flags = ReadFlags.None)
+        {
+            ReadOnlySpan<byte> value = default;
+            try
+            {
+                value = GetSpan(key, flags);
+
+                if (value.IsNull()) return null;
+                return value.ToArray();
+            }
+            finally
+            {
+                DangerousReleaseMemory(value);
+            }
+        }
+
+        public Span<byte> GetSpan(scoped ReadOnlySpan<byte> key, ReadFlags flags = ReadFlags.None)
+        {
+            return mainDb.GetSpanWithColumnFamily(key, columnFamily, options);
+        }
+
+        public void DangerousReleaseMemory(in ReadOnlySpan<byte> span)
+        {
+            mainDb.DangerousReleaseMemory(span);
+        }
+
+        public (bool, int) TryGetSpanCached(scoped ReadOnlySpan<byte> key, Span<byte> outSpan)
+        {
+            return GetWithSpanOutput(key, outSpan, columnFamily, cachedOptions);
+        }
+
+        private unsafe (bool, int) GetWithSpanOutput(ReadOnlySpan<byte> key, Span<byte> output, ColumnFamilyHandle? cf, ReadOptions readOptions)
+        {
+            nint dbh = mainDb._db.Handle;
+            nint read_options = readOptions.Handle;
+            UIntPtr skLength = (UIntPtr)key.Length;
+            IntPtr handle;
+            IntPtr errPtr;
+            fixed (byte* keyPtr = &MemoryMarshal.GetReference(key))
+            {
+                handle = cf is null
+                    ? Native.Instance.rocksdb_get_pinned(dbh, read_options, keyPtr, skLength, out errPtr)
+                    : Native.Instance.rocksdb_get_pinned_cf(dbh, read_options, cf.Handle, keyPtr, skLength, out errPtr);
+            }
+
+            if (errPtr != IntPtr.Zero)
+            {
+                // Ugh.. string check!
+                string? err = Marshal.PtrToStringAnsi(errPtr);
+                if (err! == "Result incomplete: no blocking io" || err == "Result incomplete: Table not found in table_cache, no_io is set")
+                {
+                    return (false, 0);
+                }
+                ThrowRocksDbException(errPtr);
+            }
+            if (handle == IntPtr.Zero) return (false, 0);
+
+            try
+            {
+                IntPtr valuePtr = Native.Instance.rocksdb_pinnableslice_value(handle, out UIntPtr valueLength);
+                if (valuePtr == IntPtr.Zero)
+                {
+                    return (true, 0);
+                }
+
+                int length = (int)valueLength;
+                new ReadOnlySpan<byte>((void*)valuePtr, length).CopyTo(output);
+                return (true, length);
+            }
+            finally
+            {
+                Native.Instance.rocksdb_pinnableslice_destroy(handle);
+            }
+
+            [DoesNotReturn, StackTraceHidden]
+            static unsafe void ThrowRocksDbException(nint errPtr)
+            {
+                string? err = Marshal.PtrToStringAnsi(errPtr);
+                Console.Error.WriteLine($"The err is {err}");
+                throw new RocksDbException(errPtr);
+            }
+        }
+
+        public byte[]? FirstKey
+        {
+            get
+            {
+                using Iterator iterator = mainDb.CreateIterator(options);
+                iterator.SeekToFirst();
+                return iterator.Valid() ? iterator.GetKeySpan().ToArray() : null;
+            }
+        }
+
+        public byte[]? LastKey
+        {
+            get
+            {
+                using Iterator iterator = mainDb.CreateIterator(options);
+                iterator.SeekToLast();
+                return iterator.Valid() ? iterator.GetKeySpan().ToArray() : null;
+            }
+        }
+
+        public ISortedView GetViewBetween(ReadOnlySpan<byte> firstKey, ReadOnlySpan<byte> lastKey)
+        {
+            ReadOptions readOptions = new ReadOptions();
+            readOptions.SetSnapshot(snapshot);
+
+            unsafe
+            {
+                IntPtr iterateLowerBound = Marshal.AllocHGlobal(firstKey.Length);
+                firstKey.CopyTo(new Span<byte>(iterateLowerBound.ToPointer(), firstKey.Length));
+                Native.Instance.rocksdb_readoptions_set_iterate_lower_bound(readOptions.Handle, iterateLowerBound, (UIntPtr)firstKey.Length);
+
+                IntPtr iterateUpperBound = Marshal.AllocHGlobal(lastKey.Length);
+                lastKey.CopyTo(new Span<byte>(iterateUpperBound.ToPointer(), lastKey.Length));
+                Native.Instance.rocksdb_readoptions_set_iterate_upper_bound(readOptions.Handle, iterateUpperBound, (UIntPtr)lastKey.Length);
+            }
+
+            Iterator iterator = mainDb.CreateIterator(readOptions, columnFamily);
+            return new RocksdbSortedView(iterator);
+        }
+
         public void Dispose()
         {
             snapshot.Dispose();
